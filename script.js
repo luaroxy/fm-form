57607c7c78fabf7b23973abf604342410e0040a2
luaroxy <luannamsiqueira@gmail.com>
2022-08-23 22:19:04 -0600

    Started validation if empty

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+function validateForm(e) {
+  const form = e.target;
+  const field = Array.from(form.elements);
 
+  // reset fields
+  field.forEach((i) => {
+    i.setCustomValidity("");
+    i.parentElement.classList.remove("invalid");
+  });
+
+  if (!form.checkValidity()) {
+    // form is invalid - cancel submit
+    e.preventDefault();
+    e.stopImmediatePropagation();
+
+    // apply invalid class
+    field.forEach((i) => {
+      if (!i.checkValidity()) {
+        // field is invalid - add class
+        i.parentElement.classList.add("invalid");
+      }
+    });
+  }
+}
+
+const form = document.getElementById("ctaForm");
+form.noValidate = true;
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  validateForm(e);
+});

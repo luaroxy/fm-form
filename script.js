d72d2267d4b27d9781280ae0aeb34147a9e036e7
luaroxy <luannamsiqueira@gmail.com>
2022-08-27 22:47:54 -0600

    Added name and email inline validation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function validateForm(e) {
+/*function validateForm(e) {
   const form = e.target;
   const field = Array.from(form.elements);
 
@@ -28,4 +28,69 @@ form.noValidate = true;
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   validateForm(e);
+});
+*/
+
+const validators = {
+  required: (element) => element.value.length > 0,
+  noNumbers: (element) => !element.value.match(/[0-9]/g),
+  validEmail: (element) => element.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/),
+};
+
+function validateElement(element) {
+  resetValidation(element);
+  const rules = element.dataset.validate.split(" ");
+  rules.forEach((rule) => {
+    if (validators[rule](element)) {
+      return;
+    } else {
+      resetValidation(element);
+      markElementInvalid(element, rule);
+    }
+  });
+}
+
+function markElementInvalid(element, validatorName) {
+  element.classList.add("invalid");
+  const feedbackMessage = element.parentNode.querySelector(
+    `[data-validation-message=${validatorName}]`
+  );
+  feedbackMessage.classList.add("message-visible");
+}
+
+const form = document.getElementById("ctaForm");
+form.noValidate = true;
+const formElements = Array.from(form.elements);
+
+formElements.forEach((formElement) => {
+  formElement.addEventListener("blur", () => {
+    validateElement(formElement);
+  });
+});
+
+function resetValidation(element) {
+  element.classList.remove("invalid");
+  element.parentNode
+    .querySelectorAll("[data-validation-message]")
+    .forEach((e) => {
+      e.classList.remove("message-visible");
+    });
+}
+
+form.addEventListener("submit", (event) => {
+  let formIsValid = true;
+  form.classList.remove("invalid");
+
+  formElements.forEach((formElement) => {
+    if (!formElement.dataset) return;
+    if (!formElement.dataset.validate) return;
+    validateElement(formElement);
+  });
+
+  formIsValid = form.querySelectorAll(".invalid").length === 0;
+
+  if (formIsValid === false) {
+    form.classList.add("invalid");
+    event.preventDefault();
+  }
 });
